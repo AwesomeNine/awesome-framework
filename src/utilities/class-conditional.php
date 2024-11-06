b2de1150a6f684359f4cfb147171ad2f9ffe2a52
Shakeeb Ahmed <me@shakeebahmed.com>
2024-11-06 20:37:37 +0300

    Add sanitize class

--- src/utilities/class-conditional.php
+++ src/utilities/class-conditional.php
@@ -17,15 +17,6 @@ defined( 'ABSPATH' ) || exit;
 class Conditional {
 
 	/**
-	 * Determines whether the current request is an autosave
-	 *
-	 * @return bool
-	 */
-	public static function wp_doing_autosave(): bool {
-		return defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE;
-	}
-
-	/**
 	 * Is REST request
 	 *
 	 * @return bool
@@ -37,5 +28,25 @@ class Conditional {
 		}
 
 		return false !== strpos( $request, trailingslashit( rest_get_url_prefix() ) );
+	}
+
+	/**
+	 * Checks if the current request is an AJAX request.
+	 * It can be a request to `admin-ajax.php` or to `ajax-handler.php`.
+	 *
+	 * @return bool
+	 */
+	public static function doing_ajax(): bool {
+		return wp_doing_ajax() || 'XMLHttpRequest' === Params::server( 'HTTP_X_REQUESTED_WITH' );
+	}
+
+
+	/**
+	 * Determines whether the current request is an autosave
+	 *
+	 * @return bool
+	 */
+	public static function doing_autosave(): bool {
+		return defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE;
 	}
 }
