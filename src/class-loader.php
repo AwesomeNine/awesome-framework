a5a933720c22bef6b885819f527715a5889f2937
Shakeeb Ahmed <me@shakeebahmed.com>
2026-02-27 21:35:19 +0300

    Revert "Add custom priority for integration"
    This reverts commit a7ae980a8cc1585410c9664d8e21ff32b0b5cf90.

--- src/class-loader.php
+++ src/class-loader.php
@@ -45,13 +45,6 @@ class Loader {
 	protected array $containers = [];
 
 	/**
-	 * Custom containers.
-	 *
-	 * @var array<int, mixed>
-	 */
-	protected array $customs = [];
-
-	/**
 	 * Magic getter for containers.
 	 *
 	 * @param string $name Name of property.
@@ -84,11 +77,6 @@ class Loader {
 	public function load(): void {
 		$this->load_initializers();
 
-		$custom_priorities = array_keys( $this->customs );
-		foreach ( $custom_priorities as $priority ) {
-			\add_action( 'init', [ $this, 'load_customs' ], $priority );
-		}
-
 		if ( ! \did_action( 'init' ) ) {
 			\add_action( 'init', [ $this, 'load_integrations' ] );
 		} else {
@@ -112,27 +100,14 @@ class Loader {
 	 * @return void
 	 */
 	private function register( string $register_as, $class_name, string $alias = '', $args = null ): void {
-		$target =& $this->{$register_as};
-
-		if ( ! empty( $args ) && isset( $args['priority'] ) ) {
-			$priority = $args['priority'];
-
-			if ( ! isset( $this->customs[ $priority ] ) ) {
-				$this->customs[ $priority ] = [];
-			}
-
-			unset( $args['priority'] );
-			$target =& $this->customs[ $priority ];
-		}
-
 		if ( ! empty( $args ) ) {
 			$class_name = [ $class_name, $args ];
 		}
 
 		if ( '' === $alias ) {
-			$target[] = $class_name;
+			$this->{$register_as}[] = $class_name;
 		} else {
-			$target[ $alias ] = $class_name;
+			$this->{$register_as}[ $alias ] = $class_name;
 		}
 	}
 
@@ -205,22 +180,6 @@ class Loader {
 	public function load_routes(): void {
 		foreach ( $this->routes as $alias => $class ) {
 			$this->create_container( $class, 'register_routes', $alias );
-		}
-	}
-
-	/**
-	 * Loads all registered customs if their conditionals are met.
-	 *
-	 * @return void
-	 */
-	public function load_customs(): void {
-		global $wp_filter;
-
-		$hook     = current_filter();
-		$priority = $wp_filter[ $hook ]->current_priority();
-
-		foreach ( $this->customs[ $priority ] as $alias => $class ) {
-			$this->create_container( $class, 'hooks', $alias );
 		}
 	}
 
